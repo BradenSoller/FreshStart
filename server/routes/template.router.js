f1b170f23f7231e1483a8db18ba7b829ce4ae419
Braden Soller <braden.soller18@gmail.com>
2025-08-01 17:04:08 -0500

    created get route to get item info

--- server/routes/template.router.js
+++ server/routes/template.router.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {
+  rejectUnauthenticated,
+} = require("../modules/authentication-middleware");
+const cloudinaryUpload = require("../modules/cloudinary.config");
 
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
-  // GET route code here
-});
+router.get('/items', (req, res) => {
+  const getItemsDetails =
+    `
+  SELECT * FROM "Additem"
+    
+  `
+ 
+  pool.query(getItemsDetails)
+    .then(result => {
 
+      res.send(result.rows);
+   
+    })
+    .catch((err) => {
+      console.log("ERROR: Get all appointments", err);
+      res.sendStatus(500);
+    });
+})
 /**
  * POST route template
  */
