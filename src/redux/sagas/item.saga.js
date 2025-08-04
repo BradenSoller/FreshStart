80e88748caf7ffbfa2f5b1dbfe84dc78b1676277
Braden Soller <braden.soller18@gmail.com>
2025-08-04 08:35:35 -0500

    set up saga for receving item info and set up reducer for storing item info

---
+++ src/redux/sagas/item.saga.js
@@ -0,0 +1,18 @@
+import { put, take, takeLatest } from "redux-saga/effects";
+import axios from "axios";
+
+ function* getAllItems() {
+        try {
+          const response = yield axios({
+            method: "GET",
+            url: "/api/Items/Items",
+          });
+          yield put({
+            type: "SET_ITEMS",
+            payload: response.data,
+          });
+        } catch (error) {
+          console.log("Unable to get pending events from server", error);
+        }
+
+ }
