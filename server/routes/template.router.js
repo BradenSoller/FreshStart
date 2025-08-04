d48b1fe03c96cb9c876156b2a7ab5359578445e4
Braden Soller <braden.soller18@gmail.com>
2025-08-04 08:28:43 -0500

    added post route

--- server/routes/template.router.js
+++ server/routes/template.router.js
@@ -30,8 +30,50 @@ router.get('/items', (req, res) => {
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
-  // POST route code here
+router.post('/', cloudinaryUpload.single("image"), async (req, res) => {
+  try {
+    // Check if the file exists
+    if (!req.file) {
+      return res.status(400).send("No file uploaded.");
+    }
+
+    const fileUrl = req.file.path; // Cloudinary URL of the uploaded file
+    const title = req.body.title;
+    const description = req.body.description;
+    const size = req.body.size;
+
+    console.log("fileUrl", fileUrl);
+    
+    // Ensure the title is provided
+    if (!title) {
+      return res.status(400).send("Title is required.");
+    }
+
+    // SQL query for inserting the anime data
+    const query = `
+      INSERT INTO "AddItem" ("title","description","image","size")
+      VALUES ($1, $2, $3, $4)
+      RETURNING "id", "title","description","image","size";
+    `;
+    const values = [title, description, size, fileUrl];
+
+    // Execute the query
+    const result = await pool.query(query, values);
+
+    // If insert is successful, return the anime details
+    if (result.rows.length > 0) {
+      res.status(201).json({
+        message: "Item added successfully",
+        anime: result.rows[0], // Include the anime details in the response
+      });
+    } else {
+      res.status(500).send("Failed to add anime");
+    }
+  } catch (err) {
+    console.error('POST route failed:', err);
+    res.status(500).send("Server error");
+  }
 });
+
 
 module.exports = router;
